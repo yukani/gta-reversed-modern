346ab17e1356e35ce68318899092d2b120ae5ff6
yukani <kurbagatengu@gmail.com>
2026-02-19 00:42:50 +0300

    setup cmake for doxygen

--- setup.py
+++ setup.py
@@ -5,16 +5,18 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 AP = argparse.ArgumentParser(description="gta-reversed project setup script")
 AP.add_argument("--no-unity-build", action="store_true", help="disable unity build")
 AP.add_argument("--buildconf", default="Debug", choices=["Debug", "Release", "RelWithDebInfo"], help="cmake compilation type")
+AP.add_argument("--conan-opts", default="", help="extra conan options")
+AP.add_argument("--cmake-opts", default="", help="extra cmake options")
 args = AP.parse_args()
 
 try:
     subprocess.run(
-        f'conan install {SCRIPT_DIR} --build missing -s build_type="{args.buildconf}" --profile {SCRIPT_DIR}/conanprofile.txt',
+        f'conan install {SCRIPT_DIR} --build missing -s build_type="{args.buildconf}" --profile {SCRIPT_DIR}/conanprofile.txt {args.conan_opts}',
         shell=True, check=True
     )
 
     subprocess.run(
-        f'cmake --preset default{'-unity' if not args.no_unity_build else ''} {SCRIPT_DIR}',
+        f'cmake --preset default{'-unity' if not args.no_unity_build else ''} {SCRIPT_DIR} {args.cmake_opts}',
         shell=True, check=True
     )
 except subprocess.CalledProcessError as e:
